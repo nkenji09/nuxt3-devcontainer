0803dc24b1eced2c576ef0813e6a88b7ff4fbe32
Kenji Nakanishi <nakanishikenji0929@gmail.com>
2023-09-15 16:26:54 +0000

    fix(ssh): ssh/config から macOS 固有設定である「UseKeychain」を削除

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,16 @@
 # global install
 sudo npm i -g nuxi
 
+USERNAME=$(whoami)
+
+# macOS固有設定を削除
+sudo cp -r /home/${USERNAME}/.ssh_copy/* /home/${USERNAME}/.ssh
+sudo sed -i '/UseKeychain/d' /home/${USERNAME}/.ssh/config
+sudo chmod -R 600 /home/${USERNAME}/.ssh
+sudo chmod 700 /home/${USERNAME}/.ssh
+
+cd /workspace
+
 # 初回のみ自動で node_modules を取得する
 [ ! -d \"node_modules\" ] && sudo chown -R node:node node_modules
 npm ci
